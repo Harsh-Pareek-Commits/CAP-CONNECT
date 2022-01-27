d73038a88e4c170174364dc4584833f7f048c4c0
Harsh Pareek <pareekharsh123@gmail.com>
2022-01-27 15:21:15 +0530

    first commit

---
+++ src/main/java/com/cap/capconnect/CapConnectApplication.java
@@ -0,0 +1,13 @@
+package com.cap.capconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cap/capconnect/CapConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.capconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
